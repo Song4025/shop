c414c420c506a92ebe4fe79c32a88a523ff862d3
Song4025 <thdgus4025@gmail.com>
2022-05-26 00:27:03 +0900

    add store

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import { Navbar, Container, Nav, Row, Col, Button } from 'react-bootstrap';
 import bg from './img/bg.png';
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 import data from './data.js';
 import Detail from './pages/Detail.js';
-import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom';
+import {Routes, Route, useNavigate, Outlet} from 'react-router-dom';
 import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Cart from './routes/Cart.js'
+
+// state 보관함 ContextAPI... 그런데 성능이슈때매 실전에선 안써요
+export let Context1 = createContext()
 
 function App() {
 
-  let [shoes, setShoes] = useState(data);
+  let [shoes, setShoes] = useState(data)
   let navigate = useNavigate();
-  let [탭 , 탭변경]= useState(0);
+  let [재고]= useState([10, 11, 12])
 
   return (
     <div className="App">
@@ -22,7 +25,8 @@ function App() {
         <Navbar.Brand className='Title' style={{color: 'white'}}>Navbar</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
-          <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
+          <Nav.Link onClick={()=>{navigate('/detail/0')}}>Detail</Nav.Link>
+          <Nav.Link onClick={()=>{navigate('/cart')}}>Cart</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
@@ -31,7 +35,8 @@ function App() {
       <Route path="/" element={
         <>
         <div className='main-bg' style={{backgroundImage : 'url(' + bg + ')'}}></div>
-        <Row>{
+        <Row sm={3}>
+          {
           shoes.map(((a, i)=>{
             return(
               <Card shoes={shoes[i]} i={i} key={i}></Card>
@@ -39,27 +44,7 @@ function App() {
             ))
           }
         </Row>
-        </>
-      } />
-        {/* URL 파라미터쓰면 여러페이지 만들 수 있음 */}
-        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
-        
-        {/* 아래처럼 Route안에 Route넣는걸 Nested Route라고함
-        1. 장점: 소스가 간단해짐
-        2. 장점: element 여러개를 한페이지안에 보여줄 수 있음. <Outlet>으로..
-        언제씀? 여러가지 페이지를 보여줘야하는데 페이지마다 차이가 별로없으면.
-        */}
-        <Route path="/about" element={<About />}>
-          <Route path="member" element={<div>멤버임</div>} />
-          <Route path="location" element={<div>위치정보임</div>} />
-        </Route>
-        <Route path="/event" element={<EventPage/>}>
-          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}></Route>
-          <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
-        </Route>
-        <Route path="*" element={<div>없는페이지요</div>} />
-      </Routes>
-      <Button variant="secondary" onClick={()=>{
+        <Button variant="secondary" onClick={()=>{
         // axios로 요청하는거 해볼래
         axios.get('https://codingapple1.github.io/shop/data2.json')
         .then((결과)=>{
@@ -83,49 +68,35 @@ function App() {
         // ajax를 여러군대로 보내고싶어요.
         //Promise.all([axios.get('/url1'), axios.get('url2')]).then(()=>{}) then은 생략가능
       }}>더보기</Button>
+        </>
+      } />
+        {/* URL 파라미터쓰면 여러페이지 만들 수 있음 */}
+        <Route path="/detail/:id" element={
+        <Context1.Provider value={{재고, shoes}}>
+          <Detail shoes={shoes} />
+        </Context1.Provider>
+        } />
+        <Route path="/cart" element={<Cart />} />
+        
+        {/* 아래처럼 Route안에 Route넣는걸 Nested Route라고함
+        1. 장점: 소스가 간단해짐
+        2. 장점: element 여러개를 한페이지안에 보여줄 수 있음. <Outlet>으로..
+        언제씀? 여러가지 페이지를 보여줘야하는데 페이지마다 차이가 별로없으면.
+        */}
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<div>위치정보임</div>} />
+        </Route>
+        <Route path="/event" element={<EventPage/>}>
+          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}></Route>
+          <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
+        </Route>
+        <Route path="*" element={<div>없는페이지요</div>} />
+      </Routes>
+      
       <div style={{margin:"20px"}}></div>
-      <Nav variant="pills" defaultActiveKey="1" className='NavTab'>
-      <Nav.Item>
-        <Nav.Link eventKey="1" title="Item" onClick={()=>{
-          탭변경(0)
-        }}>
-          NavLink 1 content
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="2" title="Item" onClick={()=>{
-          탭변경(1)
-        }}>
-          NavLink 2 content
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="3" title="Item" onClick={()=>{
-          탭변경(2)
-        }}>
-          NavLink 3 content
-        </Nav.Link>
-      </Nav.Item>
-      </Nav>
-      <TabContent 탭={탭}></TabContent>
-      {/* 
-      1. html/css로 미리 디자인
-      2. ui의 상태를 저장해 둘 state 만들기
-      3. component로 만들어서 적용 
-      */}
     </div>
   );
-}
-function TabContent({탭, props2}){
-  // if(탭 == 0){
-  //   return(<div>여기1</div>)
-  // }else if(탭 == 1){
-  //   return(<div>여기2</div>)
-  // }else if(탭 == 2){
-  //   return(<div>여기3</div>)
-  // }
-  // 그런데 만약 if문이 길어지는게 싫다면 아래 방법도있음.
-  return([<div>여기1</div>,<div>여기2</div>,<div>여기3</div>][탭])
 }
 
 function About(){
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 import styled from "styled-components";
+import { Context1 } from './../App.js'
 
 // class Detail2 extends React.Component{
 //   componentDidMount(){
@@ -26,11 +28,13 @@ let NewBox = styled.div`
 function Detail(props){
   
   // 언제쓰면 좋을까요? 렌더링이 되고나서 실행되어야 할 코드를 작성하면 좋음.
+  let [fade2, setFade2] = useState('');
   useEffect(()=>{
-    for(let i = 0; i < 5000; i++){
-      console.log(1);
+    setFade2('end')
+    return()=>{
+      setFade2('')
     }
-  })
+  },[])
 
   let [count, setCount] = useState(0);
   let {id} = useParams();
@@ -38,16 +42,18 @@ function Detail(props){
   let 찾은상품 = props.shoes.find(function(p){
     return p.id == id
   });
-  let [alert, setAlert] = useState(true);
+  let [탭 , 탭변경]= useState(0);
+  let [alert, setAlert] = useState(true)
+
+  // 보관함 해체 함수
+  let {재고} = useContext(Context1)
   
   useEffect(()=>{
     let a = setTimeout(()=>{ setAlert(false) }, 2000)
-    console.log(2);
     return() =>{
       // useEffect가 실행되기전에 실행하고 싶으면 여기에 작성
       // ex) 기존타이머는 제거해주세요. , 기존데이터는 제거해주세요.
       clearTimeout(a);
-      console.log(1);
     }
   }, [])
 
@@ -85,10 +91,9 @@ function Detail(props){
         <div className="alert alert-warning">2초 이내에 구매시 할인</div> 
         : null
       } 
-    <div className="container">
+    <div className={'container start ' + fade2} >
       {/* <Box bg="blue">여기에 글씨가있으면?</Box>
       <NewBox bg="blue">여기에 글씨가있으면?</NewBox> */}
-
       <button onClick={()=>{
         setCount(count + 1)
       }}>리렌더링</button>
@@ -104,6 +109,76 @@ function Detail(props){
         </div>
       </div>
     </div> 
+    <Nav variant="pills" defaultActiveKey="1" className='NavTab'>
+      <Nav.Item>
+        <Nav.Link eventKey="1" title="Item" onClick={()=>{
+          탭변경(0)
+        }}>
+          NavLink 1 content
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="2" title="Item" onClick={()=>{
+          탭변경(1)
+        }}>
+          NavLink 2 content
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="3" title="Item" onClick={()=>{
+          탭변경(2)
+        }}>
+          NavLink 3 content
+        </Nav.Link>
+      </Nav.Item>
+      </Nav>
+      <TabContent 탭={탭} />
+      {/* 
+      1. html/css로 미리 디자인
+      2. ui의 상태를 저장해 둘 state 만들기
+      3. component로 만들어서 적용 
+      */}
+      {/* 그런데 여기까지 해보니 나의 ui가 멋대가리가 없다...
+      애니메이션 추가.
+      1. 애니메이션 동작 전 className / 후 className 만들기
+      2. className에 transition속성 추가
+      3. 원할대 2번 className을 부착
+      */}
     </>
-  )}
+  )
+}
+
+  function TabContent({탭}){
+
+    // if(탭 == 0){
+    //   return(<div>여기1</div>)
+    // }else if(탭 == 1){
+    //   return(<div>여기2</div>)
+    // }else if(탭 == 2){
+    //   return(<div>여기3</div>)
+    // }
+    // 그런데 만약 if문이 길어지는게 싫다면 아래 방법도있음.
+    let [fade, setFade] = useState('')
+    let {재고} = useContext(Context1)
+  
+    useEffect(()=>{
+      let a = setTimeout(()=>{
+        setFade('end')
+      }, 10)
+      return()=>{
+        clearTimeout(a)
+        setFade('')
+      }
+    }, [탭])
+  
+    return(
+      <div className={'start '+ fade}>
+      {[
+        <div>{재고}</div>,
+        <div>여기2</div>,
+        <div>여기3</div>
+      ][탭]}
+    </div>
+    )
+  }  
 export default Detail;  
